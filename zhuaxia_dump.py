a7b9b01dbb87c0049149876d38504b0e0c59c1ba
cocobear <cocobear.cn@gmail.com>
2009-03-20 09:04:10 +0000

    save articles in current directory

--- zhuaxia_dump.py
+++ zhuaxia_dump.py
@@ -7,7 +7,7 @@ import urllib2,urllib,cookielib,httplib
 import sys,re
 
 user = "cocobear.cn@gmail.com"
-passwd = "password"
+passwd = "123456"
 
 def tag2html(name,url,description):
     global user
@@ -23,33 +23,44 @@ def login(opener):
     url = "http://www.zhuaxia.com/passport/logon_check?/\sourceid=undefined&customerId=-1"
     body = (("email",user),("password",passwd),("persistentCookie","false"))
     ret = opener.open(url,urllib.urlencode(body)).read()
-    print ret.strip("\n\r")
+    #print ret.strip("\n\r")
 
     if int(ret) != 1:
-        print "Login error!"
+        print "登录失败"
         return -1
+    else:
+        print "登录成功"
     pg_num = 0
     base_url = "http://www.zhuaxia.com/ajaxMyTagCoreFetcher.php?tag_id=-1&pg_sz=50&pg_num=%d&desc_status=0&show_type=0&logId=24&sourceid=0"
     url= base_url % pg_num
 
+    print "获取第1页文章"
     ret = opener.open(url)
     data = ret.read()
     nums_re = '中共有<b>(\d+)</b>篇'
     nums = int(re.search(nums_re,data).group(1))
     for pg_num in range(1,nums/50+1):
-        print pg_num
+        print "获取第",pg_num+1,"页文章"
         url = base_url % pg_num
         ret = opener.open(url)
         data += ret.read()
-    """
-    f = open('url.htm','w')
-    f.write(data)
-    f.close()
-    """
+    #分隔文章
+    #r = re.compile('class="item_body"(.+?)<!--end of item_body',re.S)
+    r = re.compile('class="item_desc" style="display:none">(.+?)<div class="item_opt" id="item_opt',re.S)
+    articles = re.findall(r,data)
+
     url_re = '查看原文：(.*)" href=.* target="_blank">(.*)</a>'
     urls = re.findall(url_re,data)
-    for i in urls:
-        tag2html(i[1],i[0],None)
+    i = 0
+    print "保存文件中:"
+    for url in urls:
+        print ".",
+        tag2html(url[1],url[0],None)
+        f = file(url[1].replace('/','_')+".html",'w')
+        f.write(articles[i])
+        f.close()
+        i+=1
+    print "完成"
 
 def init():
     httplib.HTTPConnection.debuglevel  =  1 
